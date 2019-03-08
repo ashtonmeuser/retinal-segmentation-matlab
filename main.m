3940ed05ef4d87df9a78dd28f8ff902b11972af0
Ashton Meuser <ashtonmeuser@gmail.com>
2019-03-07 19:34:58 -0800

    Bracket style, remove debug lines

--- main.m
+++ main.m
@@ -6,14 +6,9 @@ filename = '01_test.tif'; % Original image file path
 
 original = imread(filename);
 inverseGreen = imcomplement(original(:, :, 2));
-inverseGreen(:, 300) = 100; % Debug
-inverseGreen(:, 350) = 255; % Debug
-inverseGreen(300, :) = 100; % Debug
-inverseGreen(350, :) = 255; % Debug
-
 lineMasks = generateMaskArray(kSize, resolution);
 func = @(n) lineScore(n, lineMasks); % Anonymous fn called by convolution
 result = convolve(inverseGreen, kSize, func);
 inverseResult = imcomplement(result);
 
-montage([inverseGreen, result, inverseResult]);
+montage([inverseGreen result inverseResult]);
