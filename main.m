5ca12984168388bf830945038d46b063e46bfa1d
Ashton Meuser <ashtonmeuser@gmail.com>
2019-03-14 12:18:26 -0700

    Preliminary masking

--- main.m
+++ main.m
@@ -2,10 +2,13 @@
 
 resolution = 15; % Line detector resolution in degrees
 kSize = 15; % Kernel size
-filename = '01_test.tif'; % Original image file path
+originalFilename = '01_test.tif'; % Original image file path
+fovMaskFilename = '01_test_mask.gif'; % Image mask file path
 
-original = imread(filename);
+original = imread(originalFilename);
+fovMask = logical(imread(fovMaskFilename));
 inverseGreen = imcomplement(original(:, :, 2));
+masked = inverseGreen .* uint8(fovMask);
 lineMasks = generateMaskArray(kSize, resolution);
 func = @(n) lineScore(n, lineMasks); % Anonymous fn called by convolution
 result = convolve(inverseGreen, kSize, func);
