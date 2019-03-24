98e5e1a6aa99118375de5d2c692adb4cce793dc8
Paul Formanek <pjformanek@gmail.com>
2019-03-24 12:15:57 -0700

    Update main2.m

--- main2.m
+++ main2.m
@@ -14,8 +14,7 @@ inverseGreen = imcomplement(greenImage);
 func2 = @(n) vectorScore(n, lineMasks, orthogMasks, origMasks);
 result2 = convolve3(inverseGreen, kSize, func2);
 
-figure;
-montage([result2(:,:,1), result2(:,:,2), result2(:,:,3)]);
+figure, montage([result2(:,:,1), result2(:,:,2), result2(:,:,3)]);
 title('Resulting vector images');
 
 meanLineScore = mean2(result2(:,:,1));
@@ -27,25 +26,21 @@ SDOrthogScore = std2(result2(:,:,2));
 meanOrigScore = mean2(result2(:,:,3));
 SDOrigScore = std2(result2(:,:,3));
 
-result3 = result2;
 result3(:,:,1) = (result2(:,:,1) - meanLineScore) .* (1/SDLineScore);
 result3(:,:,2) = (result2(:,:,2) - meanOrthogScore) .* (1/SDOrthogScore);
-result3(:,:,3) = (result3(:,:,3) - meanOrigScore) .* (1/SDOrigScore);
+result3(:,:,3) = (result2(:,:,3) - meanOrigScore) .* (1/SDOrigScore);
 
-S = result3(:,:,1); % the 'kSize' LineScore
-S0 = result3(:,:,2); % the 3pxl orthogonal LineScore
+figure, montage([result3(:,:,1)*255, result3(:,:,2)*255, result3(:,:,3)*255]);
+title('Vector images after mean/sd adjustment');
+
+S = result3(:,:,1)*255; % the 'kSize' LineScore
+S0 = result3(:,:,2)*255; % the 3pxl orthogonal LineScore
 I = result3(:,:,3)*255; % the greyscale Score
+
 groundTruth = imread('01_manual1.gif')*255;
 I = imcomplement(I);
 A = S .* S0;
 B = A .* I;
 figure;
-montage({S*255, S0*255, I, groundTruth, A, B}, 'Size', [2 3]);
+montage({S, S0, I, A, B, groundTruth}, 'Size', [2 3]);
 title(['Using kSize = ', num2str(kSize), ' and resolution = ', num2str(resolution), ' degrees']); 
-
-diff1 = (B/255)-(groundTruth/255);
-diff2 = (groundTruth/255)-(B/255);
-diff = diff1+diff2; % finding how many pixels are misclassified
-figure, imshow(diff*255); % show the difference
-accuracy =((584*565)-sum(diff, 'all'))/(584*565)*100;
-title(['Misclassified pixels, groundtruth accuracy =', num2str(accuracy)]);
