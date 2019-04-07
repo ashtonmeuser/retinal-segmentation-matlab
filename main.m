141d6a7ccaa524f2a84c59273ecc1e65c6381a72
Ashton Meuser <ashtonmeuser@gmail.com>
2019-04-07 02:16:05 -0700

    Apply weights to feature vectors

--- main.m
+++ main.m
@@ -5,6 +5,7 @@ kSize = 15; % Kernel size
 orthogonalLength = 3; % Length of orthogonal line score
 originalFilename = 'DRIVE/01_test.tif'; % Original image file path
 fovMaskFilename = 'DRIVE/01_test_mask.gif'; % Image mask file path
+weights = [1 1 1]; % Weights for features (line, orthogonal, greyscale)
 
 original = imread(originalFilename);
 fovMask = logical(imread(fovMaskFilename));
@@ -14,6 +15,8 @@ lineMasks = generateMaskArray(kSize, resolution, orthogonalLength);
 func = @(n) lineScore(n, lineMasks); % Anonymous fn called by convolution
 vectors = convolve(masked, kSize, 2, func); % First, second feature vectors
 vectors(:, :, 3) = rgb2gray(original); % Third feature vector
+weighted = bsxfun(@times, reshape(weights, [1 1 3]), vectors);
+weighted = sum(weighted, 3); % Sum weighted feature vectors
 result = vectors(:, :, 1);
 inverseResult = imcomplement(result);
 
