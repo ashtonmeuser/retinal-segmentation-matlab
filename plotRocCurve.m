b070939c572c6659c1a3932937b36a4b6b709264
Ashton Meuser <ashtonmeuser@gmail.com>
2019-04-11 00:00:40 -0700

    Fix ROC curve plot, AUC calculation

--- plotRocCurve.m
+++ plotRocCurve.m
@@ -1,16 +1,17 @@
-function [] = plotRocCurve(vectors, weights, truth)
+function [areaUnderCurve] = plotRocCurve(vectors, weights, truth)
 % Threshold vectors with range of values, plot results
 
 steps = 255;
-performance = zeros(steps, 2, 'double');
-areaUnderCurve = 0;
-for index = 1:steps
-    prediction = thresholdVectors(vectors, weights, 1 / index);
-    performance(index, :) = assess(truth, prediction, false);
-    areaUnderCurve = areaUnderCurve + performance(index, 1) / steps;
+performance = zeros(steps + 2, 2, 'double');
+
+for index = 1:steps + 2
+    prediction = thresholdVectors(vectors, weights, ((index - 2) / steps));
+    [truePositiveRate, falsePositiveRate] = assess(truth, prediction, false);
+    performance(index, :) = [truePositiveRate, falsePositiveRate];
 end
 
-plot(performance);
+areaUnderCurve = trapz(performance(:, 2), performance(:, 1)) * -1; % Account for descending X values
+plot(performance(:, 2), performance(:, 1));
 title(sprintf('ROC Curve (AUC %f)', areaUnderCurve));
 xlabel('False Positive Rate');
 ylabel('True Positive Rate');
